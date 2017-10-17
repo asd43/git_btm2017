476534e3629abfc82601d83d6d29655ce336de5a
Conor Walker <conor.walk@gmail.com>
2017-10-17 10:14:42 +0200

    Conor's commit 1

--- file_manager.py
+++ file_manager.py
@@ -7,10 +7,19 @@ def add_client(client_name):
     pass
       
 def add_transaction(debtor, creditor, amount):
-    pass
+    dca = str(debtor) + ", " + str(creditor) + ", " + str(amount) + "\n"
+    with open("transactions.txt", "a") as f:
+    	f.write(dca)
+    return ":)"
         
 def get_clients():
     pass
 
 def get_transactions():
-    pass
+    with open("transactions.txt", "r") as f:
+        trans = list(map(lambda x: x.strip().split(), f.readlines()))
+    return trans
+
+
+add_transaction("a", "b", "999")
+get_transactions()
