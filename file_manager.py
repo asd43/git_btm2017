fb2afbc677e2b8b2050e75e713d680151ab72b4d
asd43 <asd43@cam.ac.uk>
2017-10-17 09:15:36 +0100

    client names

--- file_manager.py
+++ file_manager.py
@@ -4,13 +4,19 @@ client_filename = "clients.txt"
 transaction_filename = "transactions.txt"
 
 def add_client(client_name):
-    pass
-      
+    try:
+        with open(client_filename, 'a') as fi:
+            fi.write(str(client_name) + '\n')
+        print('Client added: ' + client_name)
+
+    except:
+        print('Error: Client not added')
+
 def add_transaction(debtor, creditor, amount):
     pass
-        
+   
 def get_clients():
-    pass
+    return(list(map(lambda x:x.strip(), open(client_filename,'r').readlines())))
 
 def get_transactions():
     pass
